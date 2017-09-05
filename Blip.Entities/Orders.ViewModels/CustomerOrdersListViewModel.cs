707afe78cdd3bfec61693c579ae93d9ba11714ed
ajsaulsberry <ajsaulsberry@outlook.com>
2017-09-05 15:23:19 -0400

    Cleaning up unnecessary using statements.

--- Blip.Entities/Orders.ViewModels/CustomerOrdersListViewModel.cs
+++ Blip.Entities/Orders.ViewModels/CustomerOrdersListViewModel.cs
@@ -1,10 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using Blip.Entities.Customers;
-using Blip.Entities.Orders;
 
 namespace Blip.Entities.Orders.ViewModels
 {
--- Blip.Entities/Orders.ViewModels/OrderDisplayViewModel.cs
+++ Blip.Entities/Orders.ViewModels/OrderDisplayViewModel.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-using Blip.Entities.Customers;
-using Blip.Entities.Orders;
 
 namespace Blip.Entities.Orders.ViewModels
 {
